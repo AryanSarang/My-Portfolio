423989b0a90ac88da22d248733943f9756845745
Aryan Sarang <thearyansarang@gmail.com>
2024-07-07 19:35:38 +0530

    fix github merge issue

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -15,7 +15,7 @@ export default function Badge() {
     return (
         <Canvas camera={{ position: [0, 0, 13], fov: 25 }} gl={{ alpha: true }}>
             <ambientLight intensity={Math.PI} />
-            <Physics interpolate gravity={[0, -30, 0]} timeStep={1 / 60}>
+            <Physics interpolate gravity={[0, -20, 0]} timeStep={1 / 60}>
                 <Band />
             </Physics>
             <Environment blur={0.75}>
@@ -77,7 +77,6 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
             ang.copy(card.current.angvel())
             rot.copy(card.current.rotation())
             card.current.setAngvel({ x: ang.x, y: ang.y - rot.y * 0.15, z: ang.z })
-            card.current.setAngvel({ x: ang.x, y: ang.y - rot.y * 0.15, z: ang.z })
         }
     })
 
@@ -109,7 +108,6 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
 
                         <mesh geometry={nodes.card_0.children[0].geometry}>
                             <meshPhysicalMaterial map={materials.base.map} map-anisotropy={16} clearcoat={1} clearcoatRoughness={0.15} roughness={0.3} metalness={0.8} />
-                            <meshPhysicalMaterial map={materials.base.map} map-anisotropy={16} clearcoat={1} clearcoatRoughness={0.15} roughness={0.3} metalness={0.8} />
                         </mesh>
                         <mesh geometry={nodes.clip_1.children[0].geometry} material={materials.metal} material-roughness={0.3} />
                         <mesh geometry={nodes.clamp_2.children[0].geometry} material={materials.metal} />
@@ -117,10 +115,9 @@ function Band({ maxSpeed = 50, minSpeed = 10 }) {
                 </RigidBody>
             </group>
             <mesh ref={band} >
-                <mesh ref={band} >
-                    <meshLineGeometry />
-                    <meshLineMaterial color="white" depthTest={false} resolution={[width, height]} useMap map={texture} repeat={[-3, 1]} lineWidth={1} />
-                </mesh>
-            </>
-            )
+                <meshLineGeometry />
+                <meshLineMaterial color="white" depthTest={false} resolution={[width, height]} useMap map={texture} repeat={[-3, 1]} lineWidth={1} />
+            </mesh>
+        </>
+    )
 }
